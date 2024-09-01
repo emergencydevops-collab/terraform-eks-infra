77b1b65dddf4234d1871e062d84e44125acd7fe0
emergencydevops-collab <emergencydevops@gmail.com>
2024-09-02 00:21:36 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -3,15 +3,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = ">= 4.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+      version = ">= 2.0"
     }
   }
-}
-
-provider "aws" {
-  region = var.region
 }
