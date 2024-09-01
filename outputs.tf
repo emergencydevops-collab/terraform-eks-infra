be14509a5437c2c653ebb3f5ea102362567a1ee0
emergencydevops-collab <emergencydevops@gmail.com>
2024-09-01 23:35:59 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,10 @@
 
+output "cluster_id" {
+  description = "The ID of the EKS cluster"
+  value       = module.eks.cluster_id
+}
+
+output "node_group_role_arn" {
+  description = "The ARN of the node group role"
+  value       = module.eks.node_groups["eksdemo1-ng-public1"].role_arn
+}
