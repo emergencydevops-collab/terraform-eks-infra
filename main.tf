faf43697d5da44d2eb5ccd90d1f9b0f4a0d9a579
emergencydevops-collab <emergencydevops@gmail.com>
2024-09-02 00:22:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,54 +1,31 @@
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = var.cluster_name
-  cluster_version = var.kubernetes_version
-  cluster_endpoint_private_access = false
-  cluster_endpoint_public_access  = true
+resource "aws_eks_cluster" "eks" {
+  name     = var.cluster_name
+  version  = "1.30"
+  role_arn = aws_iam_role.eks.arn
 
-  vpc_id     = aws_vpc.main.id
-  subnet_ids = aws_subnet.public[*].id
-
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = var.desired_capacity
-      max_capacity     = var.max_size
-      min_capacity     = var.min_size
-
-      instance_type = var.instance_type
-      key_name      = var.ssh_key_name
-
-      additional_tags = {
-        Name = "eks-node-group"
-      }
-    }
+  vpc_config {
+    subnet_ids = var.subnet_ids
   }
+
+  tags = var.tags
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = var.node_group_name
+  node_role_arn   = aws_iam_role.eks_node.arn
+  subnet_ids      = var.subnet_ids
+  instance_types  = [var.instance_type]
 
-resource "aws_subnet" "public" {
-  count = 2
-  cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
-  vpc_id     = aws_vpc.main.id
-  map_public_ip_on_launch = true
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+  scaling_config {
+    desired_size = var.desired_capacity
+    max_size     = var.max_capacity
+    min_size     = var.min_capacity
   }
-}
 
-resource "aws_route_table_association" "public" {
-  count = 2
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
+  remote_access {
+    ec2_ssh_key = var.key_name
+  }
+
+  tags = var.tags
 }
